36fa696d8a8c10084aa4a078d9a5f1b0da83d505
gaurav <bairagi7477@gmail.com>
2023-02-21 21:27:10 +0530

    slider and offfer page completed

--- src/App.js
+++ src/App.js
@@ -3,13 +3,18 @@ import './App.css';
 import PreNavbar from './components/PreNavbar';
 import Navbar from './components/Navbar';
 import { BrowserRouter as Router } from 'react-router-dom';
-
+import Slider from './components/Slider';
+import data from './data/data.json'
+import Offers from './components/Offers';
 
 const App = () => {
+  // console.log("start",Alldata.banner.start)
   return (
     <Router>
       <PreNavbar />
       <Navbar />
+      <Slider start = {data.banner.start} />
+      <Offers offer={data.offer} />
     </Router>
   );
 }
